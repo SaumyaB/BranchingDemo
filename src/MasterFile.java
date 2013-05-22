1fb8326bf4c0679a0ccf79789606554b5b22ce2f
Saumya <saumya_bhatnagar@hotmail.com>
2013-05-23 03:58:46 +0530

    Edit to master file (1): Team 1

--- src/MasterFile.java
+++ src/MasterFile.java
@@ -7,6 +7,7 @@ public class MasterFile {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is the Master file. Version 1.0");
+System.out.println("Edit 1 to master file: Done by Team 1");
 	}
 
 }
