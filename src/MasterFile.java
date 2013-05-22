8534152c495a835f62c11c87d10f27b536fa14b1
Saumya <saumya_bhatnagar@hotmail.com>
2013-05-23 03:52:19 +0530

    Master File Version 1.0: Created by Team 1

---
+++ src/MasterFile.java
@@ -0,0 +1,12 @@
+
+public class MasterFile {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is the Master file. Version 1.0");
+	}
+
+}
